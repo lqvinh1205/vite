d43181986d85198195199f2bbf24f8fa72b92751
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-01-07 09:42:15 +0700

    commit firt

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const menus = ["Trang chủ", "Tuyển sinh", "Chương trình đào tạo", "Góc sinh viên", "Tuyển dụng"];
+export const news = [
+  {
+    id: 1,
+    image: "https://images.unsplash.com/photo-1572949645841-094f3a9c4c94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    title: "Vinh danh 295 sinh viên FPT Polytechnic kỳ Spring 2018",
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias nisi iusto eius, modi inventore facilis molestias id est voluptates perferendis suscipit pariatur fugit vel odit cumque",
+  },
+  {
+    id: 2,
+    image: "https://images.unsplash.com/photo-1523995462485-3d171b5c8fa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+    title: "Sinh viên khối ngành kinh tế học cách chạm vào cảm xúc",
+    content: "Với sự tham gia của các chuyên gia trong lĩnh vực kinh doanh, chương trình đã thu hút đông đảo các sinh viên FPT Polytechic tham gia.",
+  },
+  {
+    id: 1,
+    image: "https://images.unsplash.com/photo-1508612761958-e931d843bdd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80",
+    title: "Sinh viên FPT trải nghiệm thực tế ở Mường Thanh Luxury",
+    content: "Với triết lý thực hành thực nghiệp Cao đẳng FPT thường xuyên cho sinh viên đi tham quan các đơn vị doanh nghiệp",
+  },
+];
+export const actions = [
+  {
+    id: 1,
+    image: "https://images.unsplash.com/photo-1597019558926-3eef445fdf60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    title: "Vinh danh 295 sinh viên FPT Polytechnic kỳ Spring 2018",
+    content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias nisi iusto eius, modi inventore facilis molestias id est voluptates perferendis suscipit pariatur fugit vel odit cumque",
+  },
+  {
+    id: 2,
+    image: "https://images.unsplash.com/photo-1582135739731-e748a423f4fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    title: "Sinh viên khối ngành kinh tế học cách chạm vào cảm xúc",
+    content: "Với sự tham gia của các chuyên gia trong lĩnh vực kinh doanh, chương trình đã thu hút đông đảo các sinh viên FPT Polytechic tham gia.",
+  },
+  {
+    id: 1,
+    image: "https://images.unsplash.com/photo-1500375592092-40eb2168fd21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+    title: "Sinh viên FPT trải nghiệm thực tế ở Mường Thanh Luxury",
+    content: "Với triết lý thực hành thực nghiệp Cao đẳng FPT thường xuyên cho sinh viên đi tham quan các đơn vị doanh nghiệp",
+  },
+];
